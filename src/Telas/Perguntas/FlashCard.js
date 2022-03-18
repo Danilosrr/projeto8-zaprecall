ee1b9b9fd24c5c48ac5ecbaf78df6f9ef0fad012
Danilo <danilo_srr@hotmail.com>
2022-03-18 13:14:34 -0300

    flashcards content  added, show question on click added

--- src/Telas/Perguntas/FlashCard.js
+++ src/Telas/Perguntas/FlashCard.js
@@ -1,17 +1,41 @@
+import React from "react"
 
 const dadosFlashcards=[
-    { key:'pergunta1', indice:1},
-    { key:'pergunta2', indice:2},
-    { key:'pergunta3', indice:3},
-    { key:'pergunta4', indice:4},
+    { key:'pergunta1', Q: 'O que é JSX?', R: 'Uma extensão de linguagem do JavaScript'},
+    { key:'pergunta2', Q: 'O React é __', R: 'uma biblioteca JavaScript para construção de interfaces'},
+    { key:'pergunta3', Q: 'Componentes devem iniciar com __', R: 'letra maiúscula'},
+    { key:'pergunta4', Q: 'Podemos colocar __ dentro do JSX', R: 'expressões'},
+    { key:'pergunta5', Q: 'O ReactDOM nos ajuda __', R: 'interagindo com a DOM para colocar componentes React na mesma'},
+    { key:'pergunta6', Q: 'Usamos o npm para __', R: 'gerenciar os pacotes necessários e suas dependências'},
+    { key:'pergunta7', Q: 'Usamos props para __', R: 'passar diferentes informações para componentes'},
+    { key:'pergunta8', Q: 'Usamos estado (state) para __ ', R: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'}
 ]
-export {dadosFlashcards}
+
+const flashcardsEmbaralhados=dadosFlashcards.sort(()=>Math.random()-0.5)
+export {flashcardsEmbaralhados}
 
 export default function FlashCard(props){
-    return(
-        <div className="Pergunta">
-            <h1>Pergunta {props.indice}</h1>
-            <ion-icon name="play-outline"></ion-icon>
-        </div>
-    )  
+
+    const [TelaPergunta,setTelaPergunta] = React.useState(0)
+
+    function virarFlashCard(){
+        setTelaPergunta(1)
+    }
+
+    switch(TelaPergunta){
+        default:
+            return(
+                <div className="Pergunta">
+                    <h1>Pergunta {props.indice}</h1>
+                    <ion-icon onClick={virarFlashCard} name="play-outline"></ion-icon>
+                </div>
+            ) 
+        case 1:
+            return(
+                <div className="Pergunta Selecionada">
+                    <h1>{props.pergunta}</h1>
+                    <img src="assets/imgs/setinha.png" alt="ver resposta"/>
+                </div>
+            )          
+    }         
 }
--- src/Telas/Perguntas/Perguntas.js
+++ src/Telas/Perguntas/Perguntas.js
@@ -1,14 +1,16 @@
-import FlashCard, { dadosFlashcards } from "./FlashCard"
+import React from "react"
+import FlashCard, { flashcardsEmbaralhados } from "./FlashCard"
 import Logo from "./Logo"
 
 export default function Perguntas(){
+    
     return(
             <div className="pagina">
                 <div className="Perguntas">
                     <Logo/>
                     <div className="Lista">
-                        {dadosFlashcards.map(flashcards=>(
-                            <FlashCard key={flashcards.key} indice={flashcards.indice}/>
+                        {flashcardsEmbaralhados.map((flashcards,index)=>(
+                            <FlashCard key={flashcards.key} indice={index+1} pergunta={flashcards.Q} resposta={flashcards.R}/>
                         ))}
                     </div>
                     <div className="Contador">
